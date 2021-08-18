1f3a3f3ade05b6aea8ac84e02a90edf0a870acfb
Lokesh Singh <lokeshsingh.singh2@gmail.com>
2021-08-18 15:57:07 +0530

    Delete ConfigFormDAO.java

--- src/main/java/com/accolite/survey/DAO/ConfigFormDAO.java
+++
@@ -1,9 +0,0 @@
-package com.accolite.survey.DAO;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.accolite.survey.entity.SurveyFormConfig;
-
-public interface ConfigFormDAO extends MongoRepository<SurveyFormConfig,String>{
-
-}
